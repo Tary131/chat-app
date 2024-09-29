d9884f7f706119c8ddf5cfac01c267c39ee18dd3
Tary131 <taras.pincak@seznam.cz>
2024-09-29 12:16:47 +0200

    Initial commit: Set up project structure

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1MeGlLzk2TvlRgoua1WIMgsyOppMuXn0",
+    authDomain: "chat-app-3572c.firebaseapp.com",
+    projectId: "chat-app-3572c",
+    storageBucket: "chat-app-3572c.appspot.com",
+    messagingSenderId: "287277253882",
+    appId: "1:287277253882:web:c7c8c87ea8a36d724ff543"
+};
+
+const app = initializeApp(firebaseConfig);
