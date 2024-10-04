930b439025b9e67c1020ae16a8bcded35e79dfaa
Tary131 <taras.pincak@seznam.cz>
2024-10-04 13:26:02 +0200

    feat(routing): implement routing with protected and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button"
+import MainPage from '@/pages/MainPage.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './pages/ProtectedRoute';
+import RegisterForm from '@/pages/auth/Register/RegisterForm.tsx';
+import RootLayout from '@/app/layout.tsx';
+import LoginForm from '@/pages/auth/Login/LoginForm.tsx';
+
 
 const App = () => {
-    return (
-        <div>
-            <Button>Click me</Button>
-        </div>
-    );
+  const props = {
+    left: (
+      <div></div>
+    ),
+    right: <div>Right Section</div>,
+  };
+  return (
+    <RootLayout>
+      <BrowserRouter>
+        <Routes>
+          {/* Protected Route */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <MainPage {...props} />
+              </ProtectedRoute>
+            }
+          />
+          {/*Register ann Login Form */}
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
+    </RootLayout>
+  );
 };
 
 export default App;
