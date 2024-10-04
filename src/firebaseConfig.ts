8828e7701ea78f1b5750a39d8ade46e5c7fa458c
Tary131 <taras.pincak@seznam.cz>
2024-10-04 13:01:39 +0200

    feat(firebase): added Firestore and Auth initialization to Firebase setup

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,12 +1,16 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1MeGlLzk2TvlRgoua1WIMgsyOppMuXn0",
-    authDomain: "chat-app-3572c.firebaseapp.com",
-    projectId: "chat-app-3572c",
-    storageBucket: "chat-app-3572c.appspot.com",
-    messagingSenderId: "287277253882",
-    appId: "1:287277253882:web:c7c8c87ea8a36d724ff543"
+  apiKey: 'AIzaSyC1MeGlLzk2TvlRgoua1WIMgsyOppMuXn0',
+  authDomain: 'chat-app-3572c.firebaseapp.com',
+  projectId: 'chat-app-3572c',
+  storageBucket: 'chat-app-3572c.appspot.com',
+  messagingSenderId: '287277253882',
+  appId: '1:287277253882:web:c7c8c87ea8a36d724ff543',
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
