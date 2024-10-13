fa7f239728b6307509b549bb397ba1c2f6b069f5
Tary131 <taras.pincak@seznam.cz>
2024-10-13 13:53:33 +0200

    feat: implement resizable panels in MainPage component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,26 +1,39 @@
 import { FC, ReactNode } from 'react';
-//import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import ProfileSheet from "./Profile/profileSheet";
-
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from '@/components/ui/resizable';
+import ProfileSheet from '../pages/Profile/profileSheet'; 
 interface MainPageProps {
-  left: ReactNode;
-  right: ReactNode;
-
+    left: ReactNode;
+    right: ReactNode;
 }
 
 const MainPage: FC<MainPageProps> = ({ left, right }) => {
-  return (
-      <div className="h-screen grid grid-cols-5 gap-4 relative">
-          {/* Left */}
-          <div className="border-r-2 border-black col-span-1 p-4">{left}</div>
+    return (
+        <div className="h-screen relative">
+            <ResizablePanelGroup direction="horizontal">
+                {/* Left panel */}
+                <ResizablePanel defaultSize={25} className=" p-4">
+                    {left}
+                </ResizablePanel>
 
-          {/* Right */}
-          <div className="col-span-1 p-4">{right}</div>
-          <div className="absolute right-0 top-0 p-4">
-              <ProfileSheet />
-          </div>
-      </div>
-  );
+                {/* Resizable handle */}
+                <ResizableHandle withHandle />
+
+                {/* Right panel */}
+                <ResizablePanel defaultSize={75} className="p-4 ">
+                    {right}
+                </ResizablePanel>
+            </ResizablePanelGroup>
+
+            {/* Profile sheet */}
+            <div className="absolute right-0 top-0 p-4">
+                <ProfileSheet />
+            </div>
+        </div>
+    );
 };
 
 export default MainPage;
