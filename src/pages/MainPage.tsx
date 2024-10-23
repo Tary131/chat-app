44885597bb7949bd3db76cb2663f0647b7014b4b
Tary131 <taras.pincak@seznam.cz>
2024-10-23 13:45:00 +0200

    feat(MainPage): add resizable layout with profile sheet

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,39 +1,39 @@
 import { FC, ReactNode } from 'react';
 import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
 } from '@/components/ui/resizable';
-import ProfileSheet from '../pages/Profile/profileSheet'; 
+import ProfileSheet from '../pages/Profile/profileSheet';
 interface MainPageProps {
-    left: ReactNode;
-    right: ReactNode;
+  left: ReactNode;
+  right: ReactNode;
 }
 
 const MainPage: FC<MainPageProps> = ({ left, right }) => {
-    return (
-        <div className="h-screen relative">
-            <ResizablePanelGroup direction="horizontal">
-                {/* Left panel */}
-                <ResizablePanel defaultSize={25} className=" p-4">
-                    {left}
-                </ResizablePanel>
+  return (
+    <div className="h-screen relative">
+      <ResizablePanelGroup direction="horizontal">
+        {/* Left panel */}
+        <ResizablePanel defaultSize={25} >
+          {left}
+        </ResizablePanel>
 
-                {/* Resizable handle */}
-                <ResizableHandle withHandle />
+        {/* Resizable handle */}
+        <ResizableHandle withHandle />
 
-                {/* Right panel */}
-                <ResizablePanel defaultSize={75} className="p-4 ">
-                    {right}
-                </ResizablePanel>
-            </ResizablePanelGroup>
+        {/* Right panel */}
+        <ResizablePanel defaultSize={75} >
+          {right}
+        </ResizablePanel>
+      </ResizablePanelGroup>
 
-            {/* Profile sheet */}
-            <div className="absolute right-0 top-0 p-4">
-                <ProfileSheet />
-            </div>
-        </div>
-    );
+      {/* Profile sheet */}
+      <div className="absolute right-0 top-0 p-4">
+        <ProfileSheet />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
