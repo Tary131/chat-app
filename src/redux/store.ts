d5e0e2c872b3661ac93a5b15be4f33b6cfa5d3fe
Tary131 <taras.pincak@seznam.cz>
2024-10-23 13:39:50 +0200

    feat(store): configure Redux store with multiple APIs and reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authApi } from './slices/authSlice.ts';
+import { authApi } from './api/authApi.ts';
+import { chatApi } from '@/redux/api/chatApi.ts';
+import { userApi } from '@/redux/api/userApi.ts';
+import selectionReducer from '@/redux/slices/selectionSlice.ts';
+import authReducer from '@/redux/slices/authSlice.ts';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [chatApi.reducerPath]: chatApi.reducer,
+    selection: selectionReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(userApi.middleware)
+      .concat(chatApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
