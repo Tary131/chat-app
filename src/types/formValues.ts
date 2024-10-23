f36cb3e552f75208da08ef437f2b5e2ada2eee05
Tary131 <taras.pincak@seznam.cz>
2024-10-23 13:42:59 +0200

    refactor: code refactored by yar run format

--- src/types/formValues.ts
+++ src/types/formValues.ts
@@ -1,10 +1,10 @@
-export interface RegisterValues {
+export type RegisterValues = {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
-}
-export interface LoginValues {
+};
+export type LoginValues = {
   email: string;
   password: string;
-}
+};
