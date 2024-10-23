52aa2858829b2853c82c7bd80157aebac1927b3e
Tary131 <taras.pincak@seznam.cz>
2024-10-23 13:45:46 +0200

    feat(ProtectedRoute): implement user authentication check and state management

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -3,23 +3,31 @@ import { Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getAuthToken } from '@/utils/cookieUtils.ts';
+import { setAuthUserId, setFullDisplayName } from '@/redux/slices/authSlice.ts';
+import { useAppDispatch } from '@/redux/hooks/reduxHooks.ts';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const token = getAuthToken();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
       if (user && token) {
+        dispatch(setAuthUserId(user.uid));
+        dispatch(setFullDisplayName(user.displayName));
         setIsAuthenticated(true);
       } else {
         setIsAuthenticated(false);
+        dispatch(setAuthUserId(null));
+        dispatch(setFullDisplayName(null));
       }
     });
 
     return () => unsubscribe();
-  }, [token]);
+  }, [token, dispatch]);
 
   if (isAuthenticated === null) {
     return <div>Loading...</div>;
