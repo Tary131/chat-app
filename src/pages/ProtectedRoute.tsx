9e96c7a8c20013384803402e68d81dfa5b861fd7
Tary131 <taras.pincak@seznam.cz>
2024-10-28 19:50:06 +0100

    feat: add user online/offline status management in ProtectedRoute

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getAuthToken } from '@/utils/cookieUtils.ts';
 import { setAuthUserId, setFullDisplayName } from '@/redux/slices/authSlice.ts';
 import { useAppDispatch } from '@/redux/hooks/reduxHooks.ts';
+import { updateUserStatus } from '@/services/userServices';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
@@ -13,17 +13,33 @@ const ProtectedRoute = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      const handleUserOffline = async () => {
+        if (user) await updateUserStatus(user.uid, false); // Mark as offline
+      };
+
       if (user && token) {
+        // User is authenticated
         dispatch(setAuthUserId(user.uid));
         dispatch(setFullDisplayName(user.displayName));
         setIsAuthenticated(true);
+
+        // Set user as online in Firestore
+        await updateUserStatus(user.uid, true);
+
+        // Add event listener for page unload
+        window.addEventListener('beforeunload', handleUserOffline);
       } else {
+        // User is not authenticated
         setIsAuthenticated(false);
         dispatch(setAuthUserId(null));
         dispatch(setFullDisplayName(null));
       }
+
+      return () => {
+        handleUserOffline(); // Clean up: mark offline
+        window.removeEventListener('beforeunload', handleUserOffline);
+      };
     });
 
     return () => unsubscribe();
