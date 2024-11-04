f2cf2a6b157108e2305c81b6a6a20a3be9afe6ce
Tary131 <taras.pincak@seznam.cz>
2024-11-04 14:26:33 +0100

    feat(api): create user search endpoint with error handling

--- src/redux/api/injected/userApi.ts
+++ src/redux/api/injected/userApi.ts
@@ -1,18 +1,11 @@
 import { searchUsersByName } from '@/services/userServices.ts';
-import { handleFirebaseError } from '@/utils/errorHandling.ts';
+import { handleAsyncErrors} from "@/redux/api/injected/utils.ts";
 import { firebaseApi } from '@/redux/api/firebaseApi.ts';
 
 export const userApi = firebaseApi.injectEndpoints({
   endpoints: (builder) => ({
     searchUsers: builder.query({
-      async queryFn(name: string) {
-        try {
-          const users = await searchUsersByName(name);
-          return { data: users };
-        } catch (error: unknown) {
-          return handleFirebaseError(error);
-        }
-      },
+      queryFn:(name)=>handleAsyncErrors(()=>searchUsersByName(name)),
     }),
   }),
 });
